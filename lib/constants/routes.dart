a6adb2ab587207efc7f983a4c4f00862b611ea41
mdiakhate12 <mdiakhate1297@gmail.com>
2020-09-18 20:41:20 +0000

    Simplify architecture

--- lib/constants/routes.dart
+++
@@ -1,7 +0,0 @@
-class Routes {
-  static const String signupView = "signup";
-  static const String loginView = "";
-  static const String startupView = "";
-  static const String homeView = "home";
-  static const String bottomModalView = "";
-}
